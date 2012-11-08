cd44fc2303598fef657f2699a417793d82511095
Ben Woodford <ben@tweedler.co.uk>
2012-11-08 12:36:00 +0000

    No more extra libs

--- src/main/java/net/loadingchunks/plugins/SquidRegister/SquidRegister/SquidHttpManager.java
+++ src/main/java/net/loadingchunks/plugins/SquidRegister/SquidRegister/SquidHttpManager.java
@@ -1,18 +1,10 @@
 package net.loadingchunks.plugins.SquidRegister.SquidRegister;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.io.BufferedReader;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 
 public class SquidHttpManager {
 
@@ -25,38 +17,53 @@ public class SquidHttpManager {
 	
 	public String registerUser(String username, String email, String mcusername)
 	{
-		List<BasicNameValuePair> formParams = new ArrayList<BasicNameValuePair>();
-		formParams.add(new BasicNameValuePair("username", username));
-		formParams.add(new BasicNameValuePair("email", username));
-		formParams.add(new BasicNameValuePair("minecraft", username));
-		formParams.add(new BasicNameValuePair("apikey", this.plugin.getConfig().getString("api.key")));
-		
-		HttpClient http = new DefaultHttpClient();
-		HttpPost httpPost = new HttpPost(this.plugin.getConfig().getString("api.endpoint"));
-		
-		HttpResponse response;
-		
-		String output = null;
-		
+		String output = "";
 		try {
-			response = http.execute(httpPost);
-			
-			if(response == null)
-				return "failed_response";;
-			
-		    BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-		    String line = "";
-		    while ((line = rd.readLine()) != null) {
-		    	output = output + line;
-		    }
-		} catch (ClientProtocolException ex) {
-			this.plugin.getLogger().severe("An odd error happened while trying to communicat with the Apache Server!");
-			return "odd";
-		} catch (IOException ex)
+			URL url;
+		    URLConnection urlConn;
+		    DataOutputStream printout;
+		    DataInputStream input;
+		    // URL of CGI-Bin script.
+		    url = new URL(this.plugin.getConfig().getString("api.path"));
+		    // URL connection channel.
+		    urlConn = url.openConnection();
+		    // Let the run-time system (RTS) know that we want input.
+		    urlConn.setDoInput(true);
+		    // Let the RTS know that we want to do output.
+		    urlConn.setDoOutput(true);
+		    // No caching, we want the real thing.
+		    urlConn.setUseCaches(false);
+		    // Specify the content type.
+		    urlConn.setRequestProperty
+		    ("Content-Type", "application/x-www-form-urlencoded");
+		    // Send POST output.
+		    printout = new DataOutputStream(urlConn.getOutputStream ());
+	
+		    String content = "";
+		    content += "username=" + URLEncoder.encode(username, "UTF-8");
+		    content += "&email=" + URLEncoder.encode(email, "UTF-8");
+		    content += "&mcusername=" + URLEncoder.encode(mcusername, "UTF-8");
+		    content += "&key=" + URLEncoder.encode(this.plugin.getConfig().getString("api.key"), "UTF-8");
+	
+		    printout.writeBytes (content);
+		    printout.flush();
+		    printout.close();
+		    // Get response data.
+		    input = new DataInputStream(urlConn.getInputStream());
+		    String str;
+		    
+		    while (null != ((str = input.readLine())))
+		    	output += str;//System.out.println (str);
+	
+		    input.close ();
+		} catch(Exception e)
 		{
-			this.plugin.getLogger().severe("Could not connect to Apache Server to post registration details.");
-			return "disconnect";
+			e.printStackTrace();
 		}
+			
+		if(output.equals(""))
+			return "failed_response";;	
+
 			
 		if(output == null)
 			return "null";
